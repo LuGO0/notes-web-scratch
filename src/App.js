e8b3c419f5081147dccf18d040044f6b2efa30a0
Saurabh kumar <saurabhofficial@hotmail.com>
2022-11-13 23:27:12 +0530

    v1 works

---
+++ src/App.js
@@ -0,0 +1,23 @@
+const Pet = () => {
+    return React.createElement("div", {}, [
+            React.createElement("h2", {}, "heading2"),
+            React.createElement("h3", {}, "heading31"),
+            React.createElement("h3", {}, "heading32")
+        ]
+        );
+    };
+
+
+// this creates a component that can be rendered
+const App = () => { 
+    console.log("running")
+    return React.createElement(
+        "div",
+        {},
+        React.createElement("h1", {}, "foo"),
+        Pet()
+    );
+};
+
+// render it now.
+ReactDOM.render(App(), document.getElementById("root"));
