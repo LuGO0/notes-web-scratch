1b0023ec3f0e61de334cf95787b4c2d067fd1a55
Saurabh kumar <saurabhofficial@hotmail.com>
2022-11-16 00:35:38 +0530

    introducing JSX

--- src/App.js
+++ src/App.js
@@ -5,15 +5,28 @@ import ReactDOM from "react-dom";
 import Pet from "./Pet";
 
 // this creates a component that can be rendered
+
+//JS
+
+// const App = () => {
+//   console.log("running");
+//   return React.createElement(
+//     "div",
+//     {},
+//     React.createElement("h1", {}, "foo"),
+//     Pet()
+//   );
+// };
+
+// JSX
 const App = () => {
-  console.log("running");
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "foo"),
-    Pet()
-  );
-};
+    return (
+        <div>
+            <h1>foo</h1>
+            <Pet /** all properties go here */></Pet>
+        </div>
+    )
+}
 
 // render it now.
 ReactDOM.render(App(), document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,22 @@
 import React from "react";
 
+// const Pet = () => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h2", {}, "heading2"),
+//     React.createElement("h3", {}, "headisng31"),
+//     React.createElement("h3", {}, "heading32"),
+//   ]);
+// };
+
+// rewrite in Babel
 const Pet = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, "heading2"),
-    React.createElement("h3", {}, "headisng31"),
-    React.createElement("h3", {}, "heading32"),
-  ]);
-};
+    return (
+        <div>
+            <h2>heading2</h2>
+            <h3>heading31</h3>
+            <h3>heading32</h3>
+        </div>
+    )
+}
 
 export default Pet;
